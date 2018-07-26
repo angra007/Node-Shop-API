1f78ec365bf7bdcd6ae717ed30d8682ebbdff9ba
Ankit <angraankit@gmail.com>
2018-07-26 16:29:45 -0400

    Added Orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,39 +1,117 @@
 const express = require ('express');
 const route = express.Router ();
 
+const mongoose = require ('mongoose');
+const Order = require ('./../models/order')
+const Product = require ('./../models/product')
+
 route.get ('/', (req, res, next) => {
-    res.status (200).json ( {
-        message : 'Orders were fetched'
+    
+    Order.find ()
+    .select ('product quantity _id')
+    .exec().then ( (result) => {
+        
+        var response = {
+            _id : result._id,
+            count : result.length,
+            
+            order : result.map ( (doc) => {
+                return {
+                    quantity : doc.quantity,
+                    productID : doc.product,
+                    _id : doc._id,
+                    request : {
+                        type : 'GET',
+                        url :'http://localhost:3000/order/' + doc._id
+                    }
+                }
+            })
+            
+        }
+        
+        res.status (200).json (response)
+    }).catch ( (error) => {
+
     })
+    
 })
 
 route.post ('/', (req, res, next) => {
 
-    var order = {
-        productID : req.body.productID,
-        quantity : req.body.quantity
-    }
-
-    res.status (200).json ( {
-        message : 'Order was created',
-        created : order
+    Product.findById (req.body.productID).exec ().then ( (product) => {
+        
+        if (product) {
+            const order = new Order ( {
+                _id : new mongoose.Types.ObjectId (),
+                quantity : req.body.quantity,
+                product : req.body.productID
+            });
+        
+            order.save ().then ( (result) => {
+                var response = {
+                    status : 201,
+                    message : 'Successfully Created Order',
+                    order : {
+                        quantity : result.quantity,
+                            productID : result.product,
+                            _id : result._id,
+                            request : {
+                                type : 'GET',
+                                url :'http://localhost:3000/order/' + result._id
+                            }
+                    }
+                }
+                res.status (200).json (response)
+            }).catch ( (error) => {
+        
+                var response = {
+                    status : error.status | 500,
+                    message : error.message | 'Something went wrong. Please try again'
+                }
+        
+                res.status (error.status | 500).json (response)
+            });
+        }
+        else {
+            var response = {
+                status :  205,
+                message : 'Invalid Product ID'
+            }
+            res.status (205).json (response)
+        }    
+    }).catch ( (error) => {
+        var response = {
+            status : error.status | 500,
+            message : error.message | 'Something went wrong. Please try again'
+        }
+        res.status (error.status | 500).json (response)
     })
-})
+});
 
 route.get ('/:id', (req, res, next) => {
     
     const id = req.params.id;
 
-    if (id == 'special') {
-        res.status (200).json ( {
-            message : 'Handling Special ID'
-        })
-    }
-    else {
-        res.status (200).json ( {
-            message : 'Handling Other ID'
-        })
-    }
+     Order.findById (id).exec ().then ( (order) => {
+       
+        if (order) {
+            res.status (200).json (order)
+        }
+        else {
+            var response = {
+                status :  206,
+                message : 'Invalid Order ID'
+            }
+            res.status (206).json (response)
+        }    
+    }).catch ( (error) => {
+        //console.log ("error");
+        var response = {
+            status : error.status | 500,
+            message : error.message | 'Something went wrong. Please try again'
+        }
+        res.status (error.status | 500).json (response)
+    })
 })
 
 route.delete ('/:orderID', (req, res, next) => {
